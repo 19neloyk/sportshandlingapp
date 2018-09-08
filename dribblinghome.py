92b735815398f7c4349064f95776666a955eff75
Neloy Kundu <19neloyk@lrei.org>
2018-09-07 23:57:47 -0400

    Update dribblinghome.py

--- dribblinghome.py
+++ dribblinghome.py
@@ -1,3 +1,7 @@
+#I made this for soccer but this can be used with basketball, hockey, etc. 
+#Basically, in a stationary position, I move the ball with each foot in the direction (given by the app) and pull it back to the stationary position. 
+#I switch each foot when instructed to by the program.
+
 from random import randint
 import time
 x = ["^^UP^^", "^^UP^^", "^^UP^^","^^UP^^","<<SIDE>>","<<SIDE>>", "<<SIDE>>","<<SIDE>>","vvDOWNvv","vvDOWNvv","vvDOWNvv","vvDOWNvv","SWITCH SIDES"]
